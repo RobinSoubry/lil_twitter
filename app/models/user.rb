cd5fa5b419aaa5cf104677e76917315cb052fa1e
David & Justin Wong & Melissa C & Robin Soubry <pair+davidthegreat.justincadburywong.mchaparr0.robinsoubry@devbootcamp.com>
2016-05-26 22:21:52 -0700

    Update models to new schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   has_many :friendships
   has_many :followers, through: :friendships, source: :follower
 
+  # To Do
+  has_many :users_tweets
+  has_many :tweets, through: :users_tweets
 end
--- app/models/users_tweet.rb
+++ app/models/users_tweet.rb
@@ -2,6 +2,7 @@ class UsersTweet < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :user
   belongs_to :tweet
+
   has_many :tweets
   has_many :users
 end
