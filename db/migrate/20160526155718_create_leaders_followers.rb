d9d4abf3e0b1d3dbeea87add36ad8470082c3d9a
David & Justin Wong & Melissa C & Robin Soubry <pair+davidthegreat.justincadburywong.mchaparr0.robinsoubry@devbootcamp.com>
2016-05-26 20:28:11 -0700

    Debug join tables for migration files

--- db/migrate/20160526155718_create_leaders_followers.rb
+++ db/migrate/20160526155718_create_leaders_followers.rb
@@ -1,8 +1,8 @@
 class CreateLeadersFollowers < ActiveRecord::Migration
   def change
     create_table :leaders_followers do |t|
-      t.integer :leader_id, null:false
-      t.integer :follower_id, null:false
+      t.integer :leader_id
+      t.integer :follower_id
     end
   end
 end
--- db/migrate/20160526160125_create_users_tweets.rb
+++ db/migrate/20160526160125_create_users_tweets.rb
@@ -1,8 +1,5 @@
 class CreateUsersTweets < ActiveRecord::Migration
   def change
-    create_table :users_tweets do |t|
-      t.integer :retweeter_id, null: false
-      t.integer :tweet_id, null: false
-    end
+    create_join_table :users, :tweets
   end
 end
