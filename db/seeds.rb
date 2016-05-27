2d6c19b169f4c85048c10079adc7d06322ba6977
Justincadburywong <justincadburywong@gmail.com>
2016-05-26 23:15:09 -0700

    update seed data for more filling of db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,28 @@
 User.create(username: "Justin", email: "Justin@theinternet.com", password_hash: "1234")
 User.create(username: "Robin", email: "Robin@theinternet.com", password_hash: "1234")
 User.create(username: "DavidtheGreat". email: "David@theinternet.com", password_hash: "1234")
-
+User.create(username: "Mel", email: "Mel@theinternet.com", password_hash: "1234")
 
 Tweet.create(body: "It's a great day for a tweet!", tweeter_id: 1)
 Tweet.create(body: "Welcome to New York!", tweeter_id: 1)
 Tweet.create(body: "With the file db/seeds.rb, the Rails gods have given us a way of feeding default values easily and quickly to a fresh installation.", tweeter_id: 1)
+Tweet.create(body: "It's a sandwich!", tweeter_id: 1)
+Tweet.create(body: "Take me to lunch!", tweeter_id: 1)
 
 Tweet.create(body: "Literally can't even.", tweeter_id: 2)
 Tweet.create(body: "Welcome to San Francisco!", tweeter_id: 2)
 Tweet.create(body: "I said two, not too.", tweeter_id: 2)
+Tweet.create(body: "Let's get ready to rumble!", tweeter_id: 2)
+Tweet.create(body: "Somewhere over the brain flow..", tweeter_id: 2)
 
 Tweet.create(body: "Life among the trees!", tweeter_id: 3)
 Tweet.create(body: "Welcome to Jamaica!", tweeter_id: 3)
 Tweet.create(body: "Wait for it.....", tweeter_id: 3)
+Tweet.create(body: "Why did the chicken cross the road?", tweeter_id: 3)
+Tweet.create(body: "To get to the other side!!", tweeter_id: 3)
+
+Tweet.create(body: "Go ride a boat!", tweeter_id: 4)
+Tweet.create(body: "Welcome to Australia!", tweeter_id: 4)
+Tweet.create(body: "Get off the lawn you bums!", tweeter_id: 4)
+Tweet.create(body: "All boats welcome.", tweeter_id: 4)
+Tweet.create(body: "Boats boats boats.", tweeter_id: 4)
