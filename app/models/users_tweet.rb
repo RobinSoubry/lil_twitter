65a4b259886e2d09db88f608be806a91cdfd5485
David & Justin Wong & Melissa C & Robin Soubry <pair+davidthegreat.justincadburywong.mchaparr0.robinsoubry@devbootcamp.com>
2016-05-26 21:45:27 -0700

    Debug users_tweet

--- app/models/users_tweet.rb
+++ app/models/users_tweet.rb
@@ -1,5 +1,5 @@
 class UsersTweet < ActiveRecord::Base
   # Remember to create a migration!
-  belongs_to :retweeter, foreign_key: :retweeter_id, class_name: 'User'
-  belongs_to :tweet
+  belongs_to :user, foreign_key: :retweeter_id, class_name: 'User'
+  belongs_to :tweet, foreign_key: :tweeter_id
 end
