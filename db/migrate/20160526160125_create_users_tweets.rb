e885613c2fa168600027752d5eb8776a7cf6e737
David & Justin Wong & Melissa C & Robin Soubry <pair+davidthegreat.justincadburywong.mchaparr0.robinsoubry@devbootcamp.com>
2016-05-26 21:45:51 -0700

    Update miration file join table

--- db/migrate/20160526160125_create_users_tweets.rb
+++ db/migrate/20160526160125_create_users_tweets.rb
@@ -1,5 +1,8 @@
 class CreateUsersTweets < ActiveRecord::Migration
   def change
-    create_join_table :users, :tweets
+    create_table :users_tweets do |t|
+      t.integer :retweeter_id
+      t.integer :tweet_id
+    end
   end
 end
