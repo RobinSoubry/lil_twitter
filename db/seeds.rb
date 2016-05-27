80e5ebeb8f6b604de801aec3ef5d591903e89cc8
Justincadburywong <justincadburywong@gmail.com>
2016-05-26 23:08:28 -0700

    update seed data for filling db

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+User.create(username: "Justin", email: "Justin@theinternet.com", password_hash: "1234")
+User.create(username: "Robin", email: "Robin@theinternet.com", password_hash: "1234")
+User.create(username: "DavidtheGreat". email: "David@theinternet.com", password_hash: "1234")
+
+
+Tweet.create(body: "It's a great day for a tweet!", tweeter_id: 1)
+Tweet.create(body: "Welcome to New York!", tweeter_id: 1)
+Tweet.create(body: "With the file db/seeds.rb, the Rails gods have given us a way of feeding default values easily and quickly to a fresh installation.", tweeter_id: 1)
+
+Tweet.create(body: "Literally can't even.", tweeter_id: 2)
+Tweet.create(body: "Welcome to San Francisco!", tweeter_id: 2)
+Tweet.create(body: "I said two, not too.", tweeter_id: 2)
+
+Tweet.create(body: "Life among the trees!", tweeter_id: 3)
+Tweet.create(body: "Welcome to Jamaica!", tweeter_id: 3)
+Tweet.create(body: "Wait for it.....", tweeter_id: 3)
