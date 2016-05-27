d00ca6b2af7bba9aa5713084ddefe41cf4ba2380
David & Justin Wong & Melissa C & Robin Soubry <pair+davidthegreat.justincadburywong.mchaparr0.robinsoubry@devbootcamp.com>
2016-05-26 22:38:40 -0700

    Add user controller for password logic

---
+++ app/controllers/user.rb
@@ -0,0 +1,32 @@
+get '/' do
+  erb :index
+end
+
+post '/' do
+  new_user = User.new(email: params[:email], username: params[:username])
+  new_user.password= (params[:password])
+  new_user.save
+  redirect '/home'
+end
+
+get '/home' do
+  erb :success
+end
+
+
+post '/login' do
+  user = User.find_by(email: params[:email])
+  if user.authenticate?(params[:password])
+    session[:user_id] = user.id
+    redirect '/home'
+  else
+    erb :success
+  end
+end
+
+
+
+get '/logout' do
+  session[:user_id] = nil
+  erb :index
+end
